bf436ef7ec41821fcf5e41b27fb142e842114654
Juan Alejandro Grisales Navas <127368396+JuanAlejandroGrisalesNavas@users.noreply.github.com>
2023-09-13 21:37:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from basededatos import *
 from usuarios import *
-conexion = Basedatos("berry.db.elephantsql.com", "abaqcznm" ,5432, "Dk0tzRMVhrtWii2GzIoxqPlcjvq50dBn", "abaqcznm")
+
+conexion =Basedatos("berry.db.elephantsql.com", "zcogoady" ,5432, "cQlAt2_pKmQf5QGpBTr8tNyHKqGjwfVg", "zcogoady")
 conexionExitosa = conexion.conectar()
 
-usuario1 = Usuario("Pepito", "Perez", "123456789")
-usuario2 = Usuario("Andres", "Perez", "987654321")
+usuario1 = usuario("Juanito", "Navas", "2456378")
+usuario2 = usuario("Vane", "Montoya", "9786756")
 
 usuario1.imprimirNombre()
-usuario1.nombre = "Otro"
 usuario1.imprimirNombre()
 
-usuario1.crearUsuario(conexionExitosa, "Pepito", "Perez")
-#leer, editar
-#usuario1.eliminarUsuario(conexionExitosa, "Pepito", "Perez")
+#usuario1.crearUsuario(conexionExitosa, "Juanito", "Navas")
+#usuario2.crearUsuario(conexionExitosa, "Vane", "Montoya")
+
+#Crear, leer, editar y eliminar
+
+#usuario1.eliminarUsuario(conexionExitosa, "Vane", "Montoya")
 #usuario1.leerUsuarios(conexionExitosa)
+
+usuario1.buscarUsuario(conexionExitosa, "Juanito", "Navas")
