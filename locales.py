2d796eb5725e1ea5d45acecd078e26c74886d6ac
Julian David Guarnizo Castrillon <89322945+JulianG08@users.noreply.github.com>
2023-10-23 15:37:14 -0500

    Editando detalles para su correcto funcionamiento

--- locales.py
+++ locales.py
@@ -1,20 +1,16 @@
 import psycopg2
 
 class Local():
-    nombre = ""
-    producto = ""
-    cantProducto = ""
+    nombre_local = ""
 
-    def __init__(self, nombre, producto, cantProducto):
-        self.nombre = nombre
-        self.producto = producto
-        self.cantProducto = cantProducto
+    def __init__(self, nombre_local):
+        self.nombre_local = nombre_local
 
-    def crearLocal(self, conexion, nombre, producto, cantProducto):
+    def crearLocal(self, conexion, nombre_local):
         try:
             with conexion.cursor() as cursor:
-                consulta = "INSERT INTO locales(nombre, producto, cantProducto) VALUEs (%s,%s);"
-                cursor.execute(consulta, (nombre, producto, cantProducto))
+                consulta = "INSERT INTO locales(nombre_local) VALUES (%s);"
+                cursor.execute(consulta, (nombre_local,))
             conexion.commit()
             return True
         except psycopg2.Error as e:
@@ -28,26 +24,26 @@ class Local():
                 cursor.execute(consulta)
                 locales = cursor.fetchall()
                 for local in locales:
-                    print("NOMBRE: ", locales[0])
+                    print("NOMBRE:", local[0])
         except psycopg2.Error as e:
             print("Ocurrió un error al leer los locales: ", e)
 
-    def eliminarLocal(self, conexion, nombre):
+    def eliminarLocal(self, conexion, nombre_local):
         try:
             with conexion.cursor() as cursor:
-                consulta = "DELETE FROM locales WHERE nombre = %s;"
-                cursor.execute(consulta, (nombre))
+                consulta = "DELETE FROM locales WHERE nombre_local = %s;"
+                cursor.execute(consulta, (nombre_local,))
             conexion.commit()
             return True
         except psycopg2.Error as e:
             print("Ocurrió un error al eliminar el local: ", e)
             return False
         
-    def buscarLocal(self, conexion, nombre):
+    def buscarLocal(self, conexion, nombre_local):
         try:
             with conexion.cursor() as cursor:
-                consulta = "SELECT FROM locales WHERE nombre = %;"
-                cursor.execute(consulta, (nombre))
+                consulta = "SELECT * FROM locales WHERE nombre_local = %s;"
+                cursor.execute(consulta, (nombre_local,))
             conexion.commit()
             return True
         except psycopg2.Error as e:
