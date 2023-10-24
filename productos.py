0a92a26b4a77c0503abd1653d833702652d2ccc8
Julian David Guarnizo Castrillon <89322945+JulianG08@users.noreply.github.com>
2023-10-24 17:11:16 -0500

    Agregando otras funciones (probar)

--- productos.py
+++ productos.py
@@ -40,4 +40,51 @@ class Producto():
             print("Ocurrió un error al leer los productos")
     
     def eliminarProducto(self, conexion, nombre_producto, id_producto, precio_producto, disponibilidad, local_pert):
+        try:
+            with conexion.cursor() as cursor:
+                consulta = "DELETE FROM productos WHERE nombre_producto = %s;"
+                cursor.execute(consulta, (nombre_producto, id_producto, precio_producto, disponibilidad, local_pert))
+            conexion.commit()
+            return True
+        except psycopg2.Error as e:
+            print("Ocurrió un error al eliminar el producto: ", e)
+            return False
         
+    def buscarProducto(self, conexion, nombre_producto, id_producto, precio_producto, disponibilidad, local_pert):
+        try:
+            with conexion.cursor() as cursor:
+                consulta = "SELECT FROM productos WHERE nombre_producto = %s;"
+                cursor.execute(consulta, (nombre_producto, id_producto, precio_producto, disponibilidad, local_pert))
+            conexion.commit()
+            return True
+        except psycopg2.Error as e:
+            print("Ocurrió un error al buscar el producto: ", e)
+            return False
+        
+    def actualizarProducto(self, conexion, nombre_producto, id_producto, precio_producto, disponibilidad, local_pert):
+        try:
+            with conexion.cursor() as cursor:
+                cursor.execute("SELECT * FROM productos WHERE nombre_producto = %s", (nombre_producto))
+                producto = cursor.fetchone()
+                if producto:
+                    print("Producto encontrado:")
+                    print(producto)
+
+                    cambios = {}
+                    cambios["nuevo_nombre_producto"] = input("Nombre producto a modificar: ")
+                    cambios["nuevo_id_producto"] = input("Id producto a modificar: ")
+                    cambios["nuevo_precio_producto"] = input("Precio a modificar: ")
+                    
+                    confirmacion = input("¿Desea aplicar estos cambios? (S/N): ").strip().lower()
+
+                    if confirmacion == "s":
+                        with conexion.cursor() as cursor:
+                            cursor.execute("UPDATE productos SET nombre_producto = %s, id_producto = %s, precio_producto = %s WHERE nombre_producto = %s", (cambios["nuevo_nombre_producto"], cambios["nuevo_id_producto"], cambios["nuevo_precio_producto"]))
+                            conexion.commit()
+                        print("Producto actualizado exitosamenete.")
+                    else:
+                        print("Cambios no aplicados.")
+                else:
+                    print("El producto no existe")
+        except psycopg2.Error as e:
+            print("Ocurrió un error al consultar o actualizar el producto: ", e)
