4e799d4a3dfd9936480d482405f110012759163e
Julian David Guarnizo Castrillon <89322945+JulianG08@users.noreply.github.com>
2023-10-24 18:12:44 -0500

    Eliminando atributo de disponibilidad

--- productos.py
+++ productos.py
@@ -4,21 +4,19 @@ class Producto():
     nombre_producto = ""
     id_producto = ""
     precio_producto = ""
-    disponibilidad = ""
     local_pert = ""
 
-    def __init__(self, nombre_producto, id_producto, precio_producto, disponibilidad, local_pert):
+    def __init__(self, nombre_producto, id_producto, precio_producto, local_pert):
         self.nombre_producto = nombre_producto
         self.id_producto = id_producto
         self.precio_producto = precio_producto
-        self.disponibilidad = disponibilidad
         self.local_pert = local_pert
 
-    def crearProducto(self, conexion, nombre_producto, id_producto, precio_producto, disponibilidad, local_pert):
+    def crearProducto(self, conexion, nombre_producto, id_producto, precio_producto, local_pert):
         try:
             with conexion.cursor() as cursor:
-                consulta = "INSERT INTO productos(nombre_producto, id_producto, precio_producto, disponibilidad, local_pert) VALUES (%s, %s);"
-                cursor.execute(consulta, (nombre_producto, id_producto, precio_producto, disponibilidad, local_pert))
+                consulta = "INSERT INTO productos(nombre_producto, id_producto, precio_producto, local_pert) VALUES (%s, %s);"
+                cursor.execute(consulta, (nombre_producto, id_producto, precio_producto, local_pert))
             conexion.commit()
             return True
         except psycopg2.Error as e:
@@ -34,34 +32,33 @@ class Producto():
                     print("Nombre: ", producto[0])
                     print("id_producto: ", producto[1])
                     print("Precio por producto: ", producto[2])
-                    print("Disponibilidad producto: ", producto[3])
-                    print("Local: ", producto[4])
+                    print("Local: ", producto[3])
         except psycopg2.Error as e:
             print("Ocurrió un error al leer los productos")
     
-    def eliminarProducto(self, conexion, nombre_producto, id_producto, precio_producto, disponibilidad, local_pert):
+    def eliminarProducto(self, conexion, nombre_producto, id_producto, precio_producto, local_pert):
         try:
             with conexion.cursor() as cursor:
                 consulta = "DELETE FROM productos WHERE nombre_producto = %s;"
-                cursor.execute(consulta, (nombre_producto, id_producto, precio_producto, disponibilidad, local_pert))
+                cursor.execute(consulta, (nombre_producto, id_producto, precio_producto, local_pert))
             conexion.commit()
             return True
         except psycopg2.Error as e:
             print("Ocurrió un error al eliminar el producto: ", e)
             return False
         
-    def buscarProducto(self, conexion, nombre_producto, id_producto, precio_producto, disponibilidad, local_pert):
+    def buscarProducto(self, conexion, nombre_producto, id_producto, precio_producto, local_pert):
         try:
             with conexion.cursor() as cursor:
                 consulta = "SELECT FROM productos WHERE nombre_producto = %s;"
-                cursor.execute(consulta, (nombre_producto, id_producto, precio_producto, disponibilidad, local_pert))
+                cursor.execute(consulta, (nombre_producto, id_producto, precio_producto, local_pert))
             conexion.commit()
             return True
         except psycopg2.Error as e:
             print("Ocurrió un error al buscar el producto: ", e)
             return False
         
-    def actualizarProducto(self, conexion, nombre_producto, id_producto, precio_producto, disponibilidad, local_pert):
+    def actualizarProducto(self, conexion, nombre_producto, id_producto, precio_producto, local_pert):
         try:
             with conexion.cursor() as cursor:
                 cursor.execute("SELECT * FROM productos WHERE nombre_producto = %s", (nombre_producto))
