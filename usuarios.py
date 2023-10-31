4567697a3c79334a8a541520178651a22158e817
Julian David Guarnizo Castrillon <89322945+JulianG08@users.noreply.github.com>
2023-10-31 17:23:58 -0500

    Aplicando otros cambios

--- usuarios.py
+++ usuarios.py
@@ -1,13 +1,6 @@
 import psycopg2
 
-class Usuario():
-    nombre = ""
-    apellido = ""
-    documento = ""
-    usuario = ""
-    contrasena = ""
-    rol = ""
-
+class Usuario:
     def __init__(self, nombre, apellido, documento, usuario, contrasena, rol):
         self.nombre = nombre
         self.apellido = apellido
@@ -19,14 +12,15 @@ class Usuario():
     def crearUsuario(self, conexion, nombre, apellido, documento, usuario, contrasena, rol):
         try:
             with conexion.cursor() as cursor:
-                consulta = "INSERT INTO usuarios(nombre, apellido, documento, usuario, contrasena, rol) VALUES (%s, %s);"
+                consulta = "INSERT INTO usuarios (nombre, apellido, documento, usuario, contrasena, rol) VALUES (%s, %s, %s, %s, %s, %s);"
                 cursor.execute(consulta, (nombre, apellido, documento, usuario, contrasena, rol))
             conexion.commit()
+            print("Usuario creado exitosamente.")
             return True
         except psycopg2.Error as e:
-            print("Ocurrió un error al crear el usuario: ", e)
+            print("Ocurrió un error al crear el usuario:", e)
             return False
-        
+
     def leerUsuarios(self, conexion):
         try:
             with conexion.cursor() as cursor:
@@ -37,84 +31,97 @@ class Usuario():
                     print("Nombre:", usuario[0])
                     print("Apellido:", usuario[1])
                     print("Documento:", usuario[2])
-                    print("usuario: ", usuario[3])
-                    print("contraseña", usuario[4])
-                    print("Puesto", usuario[5])
+                    print("Usuario:", usuario[3])
+                    print("Contraseña:", usuario[4])
+                    print("Puesto:", usuario[5])
         except psycopg2.Error as e:
             print("Ocurrió un error al leer los usuarios:", e)
 
-    def eliminarUsuario(self, conexion, nombre, apellido, documento, usuario, contrasena, rol):
+    def eliminarUsuario(self, conexion, nombre):
         try:
             with conexion.cursor() as cursor:
-                consulta = "DELETE FROM usuarios WHERE documento = %s;"
-                cursor.execute(consulta, (nombre, apellido, documento, usuario, contrasena, rol))
+                consulta = "DELETE FROM usuarios WHERE nombre = %s;"
+                cursor.execute(consulta, (nombre))
             conexion.commit()
+            print("Usuario eliminado exitosamente.")
             return True
         except psycopg2.Error as e:
-            print("Ocurrió un error al eliminar el usuario: ", e)
+            print("Ocurrió un error al eliminar el usuario:", e)
             return False
-        
-    def buscarUsuario(self, conexion, nombre, apellido, documento, usuario, contrasena, rol):
-        try:
-            with conexion.cursor() as cursor:
-                consulta = "SELECT FROM usuarios WHERE documento = %s;"
-                cursor.execute(consulta, (nombre, apellido, documento, usuario, contrasena, rol))
-            conexion.commit()
-            return True
-        except psycopg2.Error as e:
-            print("Ocurrió un error al buscar el usuario: ", e)
-            return False    
 
-    def actualizarUsuario(self, conexion, nombre, apellido, documento, usuario, contrasena):
+    def buscarUsuario(self, conexion, documento):
         try:
             with conexion.cursor() as cursor:
-                cursor.execute("SELECT * FROM usuarios WHERE documento = %s", (documento))
+                consulta = "SELECT * FROM usuarios WHERE documento = %s;"
+                cursor.execute(consulta, (documento,))
                 usuario = cursor.fetchone()
                 if usuario:
                     print("Usuario encontrado:")
-                    print(usuario)
+                    print("Nombre:", usuario[0])
+                    print("Apellido:", usuario[1])
+                    print("Documento:", usuario[2])
+                    print("Usuario:", usuario[3])
+                    print("Contraseña:", usuario[4])
+                    print("Puesto:", usuario[5])
+                else:
+                    print("Usuario no encontrado.")
+        except psycopg2.Error as e:
+            print("Ocurrió un error al buscar el usuario:", e)
+
+    def actualizarUsuario(self, conexion, documento):
+        try:
+            with conexion.cursor() as cursor:
+                cursor.execute("SELECT * FROM usuarios WHERE documento = %s", (documento,))
+                usuario = cursor.fetchone()
+                if usuario:
+                    print("Usuario encontrado:")
+                    print("Nombre:", usuario[0])
+                    print("Apellido:", usuario[1])
+                    print("Documento:", usuario[2])
+                    print("Usuario:", usuario[3])
+                    print("Contraseña:", usuario[4])
+                    print("Puesto:", usuario[5])
 
                     cambios = {}
-                    cambios["nuevo_nombre"] = input("Nombre usuario a modificar: ")
-                    cambios["nuevo_apellido"] = input("Apellido usuario a modificar: ")
-                    cambios["nuevo_usuario"] = input("Usuario a modificar")
-                    cambios["nuevo_contraseña"] = input("contraseña a modificar")
+                    cambios["nuevo_nombre"] = input("Nuevo nombre del usuario: ")
+                    cambios["nuevo_apellido"] = input("Nuevo apellido del usuario: ")
+                    cambios["nuevo_usuario"] = input("Nuevo usuario: ")
+                    cambios["nueva_contrasena"] = input("Nueva contraseña: ")
 
                     confirmacion = input("¿Desea aplicar estos cambios? (S/N): ").strip().lower()
 
                     if confirmacion == "s":
                         with conexion.cursor() as cursor:
-                            cursor.execute("UPDATE usuarios SET nombre = %s, apellido = %s, usuario = %s, contrasena = %s, WHERE nombre = %s", (cambios["nuevo_nombre"], cambios["nuevo_apellido"], nombre, cambios["nuevo_usuario"], cambios["nuevo_contraseña"]))
+                            cursor.execute("UPDATE usuarios SET nombre = %s, apellido = %s, usuario = %s, contrasena = %s WHERE documento = %s", (cambios["nuevo_nombre"], cambios["nuevo_apellido"], cambios["nuevo_usuario"], cambios["nueva_contrasena"], documento))
                             conexion.commit()
                         print("Usuario actualizado exitosamente.")
                     else:
                         print("Cambios no aplicados.")
                 else:
-                    print("El usuario no existe")
+                    print("El usuario no existe.")
         except psycopg2.Error as e:
-            print("Ocurrió un error al consultar o actualizar el usuario: ", e)
-    
-    #Revisar
-    def login(self,conexion,usuario,contrasena):
+            print("Ocurrió un error al consultar o actualizar el usuario:", e)
+
+    def login(self, conexion, usuario, contrasena):
         try:
             with conexion.cursor() as cursor:
                 consulta = "SELECT * FROM usuarios WHERE usuario = %s AND contrasena = %s;"
                 cursor.execute(consulta, (usuario, contrasena))
                 usuario = cursor.fetchone()
                 if usuario:
-                    print("Bienvenido ", usuario[1])
+                    print("Bienvenido, " + usuario[0])
                     return True
                 else:
-                    print("Usuario o contraseña incorrectos")
+                    print("Usuario o contraseña incorrectos.")
                     return False
         except psycopg2.Error as e:
             print("Ocurrió un error al buscar el usuario:", e)
             return False
 
-    def close(self,conexion):
+    def close(self, conexion):
         try:
             conexion.close()
-            print("Conexion cerrada")
+            print("Conexión cerrada.")
         except psycopg2.Error as e:
             print("Ocurrió un error al cerrar la conexión:", e)
             return False
