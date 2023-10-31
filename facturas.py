8b65a720d2bf9348ba137ee718316d7a793cc58c
Julian David Guarnizo Castrillon <89322945+JulianG08@users.noreply.github.com>
2023-10-31 16:38:04 -0500

    Eliminando atributo metodo de pago

--- facturas.py
+++ facturas.py
@@ -3,20 +3,18 @@ from productos import Producto
 
 class Factura(Producto):
     fecha = ""
-    cant_Productos = 0  # Inicializado a 0
+    cant_Productos = 0
     mesero = ""
     mesa = ""
-    total_Pagar = 0.0  # Inicializado a 0.0
-    metodo_Pago = ""
+    total_Pagar = 0.0
 
-    def __init__(self, nombre_producto, precio_producto, local_pert, fecha, cant_Productos, mesero, mesa, total_Pagar, metodo_Pago):
+    def __init__(self, nombre_producto, precio_producto, local_pert, fecha, cant_Productos, mesero, mesa, total_Pagar):
         super().__init__(nombre_producto, precio_producto, local_pert)
         self.fecha = fecha
         self.cant_Productos = cant_Productos
         self.mesero = mesero
         self.mesa = mesa
         self.total_Pagar = total_Pagar
-        self.metodo_Pago = metodo_Pago
 
     def imprimir(self):
         print("Factura")
@@ -26,4 +24,3 @@ class Factura(Producto):
         print("Productos comprados:")
         super().leerProductos(self.conexion)  # Utiliza el método leerProductos de la clase Producto para mostrar los productos
         print("Total a pagar: $", self.total_Pagar)
-        print("Método de pago:", self.metodo_Pago)
